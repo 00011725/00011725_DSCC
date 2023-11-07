c6bb3aa857064fca3d71686c81535c2120cfe9c3
Shakhnozakhon Usmonova <00011725@wiut.uz>
2023-11-07 14:51:12 +0500

    init:First Commit

---
+++ 00011725/00011725/AppData/AppData.cs
@@ -0,0 +1,17 @@
+﻿namespace _00011725.AppData
+{
+    public class AppData
+    {
+        private readonly IConfiguration _configuration;
+
+        public AppData(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetUrl()
+        {
+            return _configuration["BaseUrl"];
+        }
+    }
+}
