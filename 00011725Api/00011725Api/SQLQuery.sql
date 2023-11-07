6b4791ddb936a9d9d260cc01bbb6a61a82e9e0cc
00011725 <91447450+00011725@users.noreply.github.com>
2023-11-07 19:12:08 +0500

    Add files via upload

---
+++ 00011725Api/00011725Api/SQLQuery.sql
@@ -0,0 +1,102 @@
+create table Book
+
+go
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('1', 'Tom and Jerry', 'Skagen', 'Avange', 'Tales');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('2', 'Alice', 'Hool', 'Novel about horror', 'Horror');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('3', 'Harry P', 'Rowling', 'Sher', 'Adventures');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('4', 'TLove', 'Hkagen', 'Happiness', 'Science fiction');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('5', 'Algernon', 'Black', 'Illiade', 'Horror');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('6', 'Good Bye', 'Rong', 'Shinny', 'Humor');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('7', 'Winter tales', 'Sen', 'Lavande', 'Tales');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('8', 'Baine', 'Kolin', 'Tain', 'Science fiction');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('9', 'Harry P', 'Rowling', 'The adventurious novel', 'Adventures');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('10', 'New Year', 'Sien', 'History', 'Humor');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('11', 'Alice', 'Hool', 'Na', 'Horror');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('12', 'Gwain', 'Green', 'Sher', 'Adventures');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('13', 'Tom and Jerry', 'Skagen', 'Stavanger', 'Tales');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('14', 'Alice', 'Hool', 'Na', 'Horror');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('15', 'Harry P', 'Rowling', 'Sher', 'Adventures');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('16', 'TLove', 'Hkagen', 'Avenge', 'Science fiction');
+
+INSERT INTO Book (Id, Title, Author, Description, Type) 
+VALUES ('17', 'Mystery', 'Smith', 'Thriller', 'Mystery');
+
+INSERT INTO Book (Id, Title, Author, Description, Type) 
+VALUES ('18', 'Science', 'Johnson', 'Exploring unknown worlds', 'Science Fiction');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('19', 'The Alchemist', 'Paulo Coelho', 'A magical story about following your dreams', 'Fantasy');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('20', 'To Kill a Mockingbird', 'Harper Lee', 'A classic novel tackling themes of racial injustice', 'Fiction');
+
+INSERT INTO Book (Id, Title, Author, Description, Type) 
+VALUES ('21', 'The Great Gatsby', 'F. Scott Fitzgerald', 'A tale of wealth, love, and the American dream', 'Fiction');
+
+INSERT INTO Book (Id, Title, Author, Description, Type) 
+VALUES ('22', '1984', 'George Orwell', 'A dystopian novel about government surveillance and control', 'Science Fiction');
+
+INSERT INTO Book (Id, Title, Author, Description, Type) 
+VALUES(('23', 'The New Year', 'George', 'A novel about New Year', 'Adventure');)
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('24', 'Algernon', 'Black', 'Illiade', 'Horror');
+
+INSERT INTO Book (Id, Title, Author, Description, Type) 
+VALUES ('25', 'War and P', 'Tolstoy', 'Historical fiction', 'Classic');
+
+INSERT INTO Book (Id, Title, Author, Description, Type) 
+VALUES ('26', 'The Hobbit', 'Tolkien', 'Fantasy adventure', 'Adventure');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('27', 'Pride and P', 'Austen', 'Romance', 'Classic');
+
+INSERT INTO Book (Id, Title, Author, Description, Type) 
+VALUES ('28', 'To Kill a M', 'Lee', 'Coming-of-age story', 'Fiction');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('29', 'The Great Escape', 'Smith', 'Thriller', 'Mystery');
+
+INSERT INTO Book (Id, Title, Author, Description, Type)
+VALUES ('30', 'The Secret Garden', 'Burnett', 'Childrens book', 'Fantasy');
+
+INSERT INTO Book (Id, Title, Author, Description, Type) 
+VALUES ('31', 'The Catcher in the Rye', 'Salinger', 'Coming-of-age novel', 'Fiction');
+
+INSERT INTO Book (Id, Title, Author, Description, Type) 
+VALUES ('32', 'To Kill a Mockingbird', 'Lee', 'Racial injustice', 'Classic');
+
+INSERT INTO Book (Id, Title, Author, Description, Type) 
+VALUES ('33', '1984', 'Orwell', 'Dystopian society', 'Science Fiction');
